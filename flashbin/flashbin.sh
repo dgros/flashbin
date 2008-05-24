16271ed6a48d85b8060370e33e9a80c9f5167297
gabriel.sallesloustau <gabriel.sallesloustau@b0221aab-e44d-0410-85a4-7fb627bd93bd>
2008-05-24 18:39:07 +0000

    implementing mount functions

--- flashbin/flashbin.sh
+++ flashbin/flashbin.sh
@@ -4,16 +4,16 @@
 # Requierements : udev, sed, awk, block2mtd module, mtd-tools package.
 # Optionnal : ubifs kernel, logfs patched kernel.
 
-# Clé a Damien
+# Cle a Damien
 #"H6LOD3Q9YL925WB0"
-#0700077A12A10233
+# 0700077A12A10233
 
 configfile="/home/gabriel/workspace/flashbin/flashbin.conf"
 
 # detect the key with udev
-udev_dem()
+flashbin_run()
 {
-echo "Detecting the key"
+echo "Detecting the key..."
 read_serial=`sed -n '/\[serial\]/,/\[\/serial\]/{//d;p}' $configfile`
 variable=`ls /sys/block | grep sd?*`
 set $variable
@@ -21,24 +21,34 @@ set $variable
 serial=1
 
 while [ -n "$1" ]
-        do
-        serial=`/usr/bin/udevinfo -a -p /sys/block/$1 | grep -m1 serial | cut -d'=' -f3 | cut -d'"' -f2`
-                        if [ -z $serial ]
-                                then
-                                        serial="erreur"
-                        fi
-                        if  [ $serial = $read_serial ]
-                                then
-                                        echo "Key found, serial = $read_serial"
-                                        create_dev $1
-#                                        break;
-                        fi
-                shift 1
-        done
+do
+	serial=`/usr/bin/udevinfo -a -p /sys/block/$1 | grep -m1 serial | cut -d'=' -f3 | cut -d'"' -f2`
+#                        if [ -z $serial ]
+#                                then
+#                                        serial="erreur"
+#					detect_flag="erreur"
+#					echo "$serial   $detect_flag"
+#                        fi
+                if  [ $serial = $read_serial ]
+                then
+                        echo "Key found, serial = $read_serial"
+			detect_flag=$1
+			break
+		else 
+			detect_flag="erreur"
+                fi
+        shift 1
+done
+
+if [ $detect_flag = "erreur" ]
+then
+	echo "No key has been detected"
+else
+	echo "Key detected as $detect_flag"
+	create_dev $detect_flag
+fi
 
 
-#echo "$1"
-echo "End of key détection"
 }
 
 
@@ -52,8 +62,17 @@ device=$1
 echo "Emulating mtd devices for your block devices"
 
 for a in `seq 0 16` ; do
-	mknod /dev/mtd$a c 90 `expr $a + $a`
-	mknod /dev/mtdblock$a b 31 $a
+
+	if [ ! -c /dev/mtd$a ]
+	then
+		mknod /dev/mtd$a c 90 `expr $a + $a`
+	fi
+
+	if [ ! -b /dev/mtdblock$a ]
+	then
+		mknod /dev/mtdblock$a b 31 $a
+	fi
+
 done	
 
 
@@ -68,12 +87,46 @@ done
 
 modprobe block2mtd $modprobe_parameters
 
+flashbin_mount
+
 }
+
+# mount filesystem
+flashbin_mount()
+{
+partitions=`sed -n '/\[partitions\]/,/\[\/partitions\]/{//d;p}' $configfile | awk '{ print $1 }' | tr '\n' ' ' `
+partitions_type=`sed -n '/\[partitions\]/,/\[\/partitions\]/{//d;p}' $configfile | awk '{ print $3 }' | tr '\n' ' ' `
+mount_point=`sed -n '/\[partitions\]/,/\[\/partitions\]/{//d;p}' $configfile | awk '{ print $2 }' | tr '\n' ' ' `
+
+#mount --rbind 
+
+}
+
+flashbin_configure()
+{
+return 0
+}
+
+flashbin_status()
+{
+return 0
+}
+
+
 
 if [ "$(id -u)" != "0" ]; then
     echo "you should run this script as root"
     exit 1
     
 else
-	udev_dem
+        if [ "$1"="" ]
+	then
+		flashbin_run
+	elif [ "$1"="configure" ]
+	then
+		flashbin_configure
+	elif [ "$1"="status" ]
+	then
+		flashbin_status
+	fi
 fi
