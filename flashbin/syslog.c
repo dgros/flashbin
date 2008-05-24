74ebbef01a842bb64e2725523b94b80ec138ab9a
gros.damien <gros.damien@b0221aab-e44d-0410-85a4-7fb627bd93bd>
2008-05-24 15:38:56 +0000

    added : gestionnaire()

--- flashbin/syslog.c
+++ flashbin/syslog.c
@@ -9,3 +9,24 @@ void sys_log(char *erreur)
 	syslog(LOG_DEBUG, erreur);
 	closelog();
 }
+
+void gestionnaire(int numero)
+{
+	switch (numero){
+		case SIGUSR1 : 
+			printf("%d recu\n");
+			break;
+		case SIGUSR2 : 
+			printf("%d recu\n");
+			break;
+
+	}
+}
+/* Correspondance de la numérotation : A faire
+ *
+ *
+ *
+ *
+ *
+ *
+ */
